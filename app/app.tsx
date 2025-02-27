7e6dfd19c912ce962e80167af1d81ba681da7634
Abdiel Cuevas <61069624+Javier13g@users.noreply.github.com>
2025-02-27 17:26:13 -0400

    Agregar estructura inicial de la aplicación con componentes de navegación, tema y página de inicio

---
+++ app/app.tsx
@@ -0,0 +1,34 @@
+import HomePage from "./home/page";
+
+export default function App() {
+    return (
+        <main className="flex flex-col flex-grow items-center justify-center">
+            {/* Aquí irán las páginas y componentes de la app */}
+            <h1 className="text-4xl font-bold">Hi, I&apos;m Abdiel Cuevas
+            </h1>
+            <h2 className="text-2xl font-bold">A results-oriented web developer who builds and manages websites and web applications that lead to overall product success.</h2>
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+            <HomePage />
+        </main>
+    );
+}
---
+++ app/home/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Hi, I&apos;m Abdiel Cuevas
+            A results-oriented web developer who builds and manages websites and web applications that lead to overall product success.</h1>
+        </div>
+    )
+}
